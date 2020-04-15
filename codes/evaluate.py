299094140cc947813200bef4413dcb21cdfb6479
yangyueren <yangyueren@outlook.com>
2020-04-16 01:45:13 +0800

    add lsh

--- codes/evaluate.py
+++ codes/evaluate.py
@@ -19,25 +19,23 @@ def all_np(arr):
         result[k] = v
     return result
 
-def cal_DB(labels, group):
+def cal_DB(labels, groups):
     """
     return the pairs set that are the same driver id
     :param labels:
     :param group:
-    :return: set : (traj1, traj2)  traj1 < traj2
+    :return: DB
     """
-    ans = set()
-    for i in range(len(group)):
-        for j in range(i):
-            if labels[i] == labels[j]:
-                assert j < i, 'pair error'
-                a = group[i]
-                b = group[j]
-                m = min(a,b)
-                n = max(a,b)
-                assert m < n, 'pair error'
-                ans.add((m, n))
-    return ans
+
+    db_dict = dict()
+    for group in groups:
+        label = labels[group]
+        cal_DB_2(label, group, db_dict)
+    DB = 0
+    for k in db_dict.keys():
+        DB += len(db_dict[k])
+
+    return DB
 
 
 def cal_DB_2(labels, group, db):
@@ -62,7 +60,7 @@ def cal_DB_2(labels, group, db):
                 db[m].add(n)
 
 
-def pc(labels, groups):
+def pc(labels, groups, DB):
     """
     Pairs Completeness (PC) assesses the portion of the duplicate entities
     that co-occur at least once in B (also known as recall in other research ﬁelds)
@@ -77,23 +75,19 @@ def pc(labels, groups):
         tmp = v * (v-1) / 2
         DE += tmp
 
-
-    db_set = set()
-    db_dict = dict()
-    for group in groups:
-        label = labels[group]
-        cal_DB_2(label, group, db_dict)
-        # cur_set = cal_DB(label, group)
-        # db_set = db_set | cur_set
-    # DB = len(db_set)
-    DB = 0
-    for k in db_dict.keys():
-        DB += len(db_dict[k])
+    # db_dict = dict()
+    # for group in groups:
+    #     label = labels[group]
+    #     cal_DB_2(label, group, db_dict)
+    #
+    # DB = 0
+    # for k in db_dict.keys():
+    #     DB += len(db_dict[k])
         
     return DB / DE
 
 
-def pq(labels, groups):
+def pq(labels, groups, DB):
     """Pairs Quality (PQ) corresponds to precision,
     as it estimates the portion of non-redundant comparisons
     that involve matching entities.
@@ -105,39 +99,47 @@ def pq(labels, groups):
     """
 
     B = 0 #不需要去重
-
-    db_set = set()
-    db_dict = dict()
     for group in groups:
         length = len(group)
         B += length*(length-1)/2
 
-        label = labels[group]
-        cal_DB_2(label, group, db_dict)
-        # cur_set = cal_DB(label, group)
-        # db_set = db_set | cur_set
-    # DB = len(db_set)
-    DB = 0
-    for k in db_dict.keys():
-        DB += len(db_dict[k])
+    # db_dict = dict()
+    # for group in groups:
+    #     length = len(group)
+    #     B += length*(length-1)/2
+    #     label = labels[group]
+    #     cal_DB_2(label, group, db_dict)
+    # DB = 0
+    # for k in db_dict.keys():
+    #     DB += len(db_dict[k])
+
     return DB / B
 
-def rr(labels, groups):
+def rr(labels, groups, DB):
     """
     Reduction Ratio (RR) estimates the portion of comparisons
     that are avoided in B with respect to the naive, brute-force approach
     :param ids: np.array, (num,)
     :param labels: np.array (num,)
     :param groups: list(list), one list (ids) represent one group
-    :return: RR(B, E)=1-||B||/||E||
+    :return: RR(B, E)=1-||B||/||E|| , replace B with DB
     """
-    B = 0
-    for group in groups:
-        length = len(group)
-        B += length * (length - 1) / 2
+    # B = 0
+    # for group in groups:
+    #     length = len(group)
+    #     B += length * (length - 1) / 2
+
+    # db_dict = dict()
+    # for group in groups:
+    #     label = labels[group]
+    #     cal_DB_2(label, group, db_dict)
+    # DB = 0
+    # for k in db_dict.keys():
+    #     DB += len(db_dict[k])
+
     num = len(labels)
     E = num * (num-1) / 2
-    return 1 - B/E
+    return 1 - DB/E
 
 def evaluate(labels, groups):
     """
@@ -146,7 +148,8 @@ def evaluate(labels, groups):
     :param groups:
     :return:
     """
-    _pc = pc(labels, groups)
-    _pq = pq(labels, groups)
-    _rr = rr(labels, groups)
+    DB = cal_DB(labels, groups)
+    _pc = pc(labels, groups, DB)
+    _pq = pq(labels, groups, DB)
+    _rr = rr(labels, groups, DB)
     return _pc, _pq, _rr
--- codes/main.py
+++ codes/main.py
@@ -6,6 +6,8 @@ import logging
 from knn import knn
 from multi_probe_lsh import multi_probe_lsh
 from evaluate import evaluate
+from lsh import LSH
+
 logger = logging.getLogger(__name__)
 logger.setLevel(level=logging.DEBUG)
 
@@ -34,21 +36,39 @@ def read_data(file_name='./data/geolife_top100.txt'):
 def frun(K=10):
     labels, features = read_data('./data/geolife_top100.txt')
     assert len(labels) == len(features), 'driver id not matches features'
-    groups_knn = knn(labels, features, K=K)
-    pc_knn, pq_knn, rr_knn = evaluate(labels, groups_knn)
-    logger.info(f',pc_knn,{pc_knn},pq_knn,{pq_knn},rr_knn,{rr_knn}')
+    # groups_knn = knn(labels, features, K=K)
+    # pc_knn, pq_knn, rr_knn = evaluate(labels, groups_knn)
+    # logger.info(f',pc_knn,{pc_knn},pq_knn,{pq_knn},rr_knn,{rr_knn}')
 
-    groups_mpl = multi_probe_lsh(labels, features, K=K)
-    pc_mpl, pq_mpl, rr_mpl = evaluate(labels, groups_mpl)
+    # groups_mpl = multi_probe_lsh(labels, features, K=K)
+    # pc_mpl, pq_mpl, rr_mpl = evaluate(labels, groups_mpl)
+    # logger.info(f',pc_mpl,{pc_mpl},pq_mpl,{pq_mpl},rr_mpl,{rr_mpl}')
+
+    lsh_index = LSH()
+    groups_lsh = multi_probe_lsh(labels, features, K=K)
+    pc_mpl, pq_mpl, rr_mpl = evaluate(labels, groups_lsh)
     logger.info(f',pc_mpl,{pc_mpl},pq_mpl,{pq_mpl},rr_mpl,{rr_mpl}')
+
+
+def grun(num_ht = 4, num_hf = 8):
+    labels, features = read_data('./data/geolife_top100.txt')
+    ids = list(range(len(labels)))
+    assert len(labels) == len(features), 'driver id not matches features'
+
+    lsh_index = LSH(num_ht, num_hf, features.shape[1])
+    lsh_index.add_data(ids, features)
+    groups_lsh = lsh_index.get_blocks()
+    pc_mpl, pq_mpl, rr_mpl = evaluate(labels, groups_lsh)
+    logger.info(f',pc_lsh,{pc_mpl},pq_lsh,{pq_mpl},rr_lsh,{rr_mpl}')
 
 
 if __name__ == '__main__':
     np.random.seed(1234)
     random.seed(1234)
-    for i in range(1, 100):
-        try:
-            frun(i*10)
-            print(f'{i*10} is done.')
-        except Exception as e:
-            print(e)
+    grun()
+    # for i in range(1, 2):
+    #     try:
+    #         frun(i*10)
+    #         print(f'{i*10} is done.')
+    #     except Exception as e:
+    #         print(e)
